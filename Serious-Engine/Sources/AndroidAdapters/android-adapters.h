2a788d9922f71f6c95bc6c79f8dcde82f7f3a600
Skyrimus <Skyrimus@yandex.ru>
2019-08-27 06:40:49 +0300

    Revert "[FIX] No more win32 synchronization, UNIX ONLY"
    This reverts commit d68cfeaf6569735f3216debaac8116df19843bc1.

--- Serious-Engine/Sources/AndroidAdapters/android-adapters.h
+++ Serious-Engine/Sources/AndroidAdapters/android-adapters.h
@@ -10,6 +10,8 @@
 #define PLATFORM_UNIX 1
 #define USE_PORTABLE_C 1
 
+static_assert(sizeof(void *) == 4, "Serious engine require 32 bit address space");
+
 int64_t getTimeNsec();
 
 unsigned int _rotl(unsigned int value, int shift);
