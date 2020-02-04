f1eb50d1dfdc1d7f71971f06bf3019e55c2f9423
Skyrimus <Skyrimus@yandex.ru>
2020-02-04 05:12:36 +0300

    No more FLEX and BISON code generation

--- cmake/generatedFiles.cmake
+++ cmake/generatedFiles.cmake
@@ -1,16 +1,3 @@
-
-# Run Flex and generate file.cpp from file.l
-function(run_flex dest source skeletonFile workdir)
-    message("flex(${source}) with skeleton '${skeletonFile}' -> ${dest}")
-
-    add_custom_command(
-            OUTPUT "${dest}"
-            COMMAND cmd /c "${PROJECT_ROOT}/Serious-Engine/Tools.Win32/Flex.exe" "-o${dest}" "-S${skeletonFile}" "${source}"
-            WORKING_DIRECTORY "${workdir}"
-            DEPENDS "${source}" "${skeletonFile}"
-    )
-endfunction()
-
 # Run ECC and generate file.cpp from file.es
 macro(run_ecc sources)
     foreach (arg IN ITEMS ${ARGN})
@@ -26,19 +13,4 @@ macro(run_ecc sources)
                 DEPENDS "${source}" "${ECC_EXECUTABLE}"
         )
     endforeach ()
-endmacro()
-
-
-# Run ECC and generate file.cpp from file.y
-macro(run_bison dest source tempFile extra)
-    message("bison(${source}) with temp '${tempFile}' -> ${dest}")
-
-    add_custom_command(
-            OUTPUT "${dest}"
-            COMMAND cmd /c "${PROJECT_ROOT}/Serious-Engine/Tools.Win32/Bison.exe" "-o${tempFile}" "${source}" -d ${extra}
-            COMMAND ${CMAKE_COMMAND} -E rename "${tempFile}" "${dest}"
-            WORKING_DIRECTORY "${SE_SOURCES}"
-            DEPENDS "${source}"
-    )
-
 endmacro()
