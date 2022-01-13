90f7a8980af510cbc75841fc4fda439242415c59
Skyrimus <Skyrimus@yandex.ru>
2022-01-13 11:39:15 +0300

    Update playerCommons.h

--- Serious-Engine/Sources/AndroidAdapters/playerCommons.h
+++ Serious-Engine/Sources/AndroidAdapters/playerCommons.h
@@ -7,59 +7,59 @@
 #define MAX_WEAPONS 30
 
 struct PlayerControls {
-    FLOAT3D aRotation;
-    FLOAT3D aViewRotation;
-    FLOAT3D vTranslation;
+	FLOAT3D aRotation;
+	FLOAT3D aViewRotation;
+	FLOAT3D vTranslation;
 
-    BOOL bMoveForward;
-    BOOL bMoveBackward;
-    BOOL bMoveLeft;
-    BOOL bMoveRight;
-    BOOL bMoveUp;
-    BOOL bMoveDown;
+	BOOL bMoveForward;
+	BOOL bMoveBackward;
+	BOOL bMoveLeft;
+	BOOL bMoveRight;
+	BOOL bMoveUp;
+	BOOL bMoveDown;
 
-    BOOL bTurnLeft;
-    BOOL bTurnRight;
-    BOOL bTurnUp;
-    BOOL bTurnDown;
-    BOOL bTurnBankingLeft;
-    BOOL bTurnBankingRight;
-    BOOL bCenterView;
+	BOOL bTurnLeft;
+	BOOL bTurnRight;
+	BOOL bTurnUp;
+	BOOL bTurnDown;
+	BOOL bTurnBankingLeft;
+	BOOL bTurnBankingRight;
+	BOOL bCenterView;
 
-    BOOL bLookLeft;
-    BOOL bLookRight;
-    BOOL bLookUp;
-    BOOL bLookDown;
-    BOOL bLookBankingLeft;
-    BOOL bLookBankingRight;
+	BOOL bLookLeft;
+	BOOL bLookRight;
+	BOOL bLookUp;
+	BOOL bLookDown;
+	BOOL bLookBankingLeft;
+	BOOL bLookBankingRight;
 
-    BOOL bSelectWeapon[MAX_WEAPONS+1];
-    BOOL bWeaponNext;
-    BOOL bWeaponPrev;
-    BOOL bWeaponFlip;
+	BOOL bSelectWeapon[MAX_WEAPONS+1];
+	BOOL bWeaponNext;
+	BOOL bWeaponPrev;
+	BOOL bWeaponFlip;
 
-    BOOL bWalk;
-    BOOL bStrafe;
+	BOOL bWalk;
+	BOOL bStrafe;
 	BOOL bStrafeFB;
-    BOOL bFire;
-    BOOL bReload;
-    BOOL bUse;
+	BOOL bFire;
+	BOOL bReload;
+	BOOL bUse;
 	BOOL bShowTabInfo;
 	BOOL bDropMoney;
-    BOOL bComputer;
-    BOOL bUseOrComputer;
-    BOOL bUseOrComputerLast;  // for internal use
-    BOOL b3rdPersonView;
+	BOOL bComputer;
+	BOOL bUseOrComputer;
+	BOOL bUseOrComputerLast;  // for internal use
+	BOOL b3rdPersonView;
 
-    BOOL bSniperZoomIn;
-    BOOL bSniperZoomOut;
-    BOOL bFireBomb;
+	BOOL bSniperZoomIn;
+	BOOL bSniperZoomOut;
+	BOOL bFireBomb;
 
-    BOOL bBack;
-    BOOL bStart;
+	BOOL bBack;
+	BOOL bStart;
 
-    float axisValue[10];
-    float shiftAxisValue[10];
+	float axisValue[10];
+	float shiftAxisValue[10];
 };
 
 #endif // PLAYERCONTROLS_H
